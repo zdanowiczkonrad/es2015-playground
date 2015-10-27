02a26630b3114c4888981b5b305e96117adf60ae
Konrad Zdanowicz <konrad@directly.com>
2015-10-27 11:43:30 +0100

    question inside question list

--- react-router/app/components/Question/QuestionList.jsx
+++ react-router/app/components/Question/QuestionList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Question from './Question';
 
 export default class QuestionList extends React.Component {
+
   render() {
     var list = ['test1', 'test2', 'test' + Math.random() ];
 
     return (
       <ul>
-        {list.map(item => <li>{item}</li>)}
+        {list.map(item => <Question content={item} />)}
       </ul>
     );
   }
